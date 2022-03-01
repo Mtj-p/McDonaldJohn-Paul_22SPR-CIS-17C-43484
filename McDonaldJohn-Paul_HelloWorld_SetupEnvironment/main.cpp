fc124b66dabe2bd53aafb016d65d1d23febc2240
jm2531972 <jay.mcdonald003@gmail.com>
2022-03-01 13:03:50 -0800

    first commit
    first commit Hello World

---
+++ McDonaldJohn-Paul_HelloWorld_SetupEnvironment/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * file: McDonaldJohn-Paul_HelloWorld_SetupEnvironment
+ * Author: John-Paul McDonald
+ * Date: 03/01/2022
+ * Purpose: Show proof of NetBeans installation 
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+//Global Constants
+//Universal Math, Physics, Conversions, Higher Dimensions
+//Prototypes
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize Random Number Seed
+    
+    //Declare Variables
+    cout<<"Hello world";
+    //Initialize Variables
+    
+    //Process the inputs -> outputs
+    
+    //Display the results, verify inputs
+    
+    //Clean up and exit
+    return 0;
+}
