b21d7f0dd2935f43330a438b3d26a5cf371ac348
jm2531972 <jay.mcdonald003@gmail.com>
2022-06-12 16:19:41 -0700

    final submitted time for project

---
+++ McDonaldJohnPaul_Final_CSC-17C_Problem7_SimpleLinked/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_1-Windows
+CND_ARTIFACT_NAME_Debug=mcdonaldjohnpaul_final_csc-17c_problem7_simplelinked
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_1-Windows/mcdonaldjohnpaul_final_csc-17c_problem7_simplelinked
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_1-Windows/package
+CND_PACKAGE_NAME_Debug=mcdonaldjohnpaulfinalcsc-17cproblem7simplelinked.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_1-Windows/package/mcdonaldjohnpaulfinalcsc-17cproblem7simplelinked.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_1-Windows
+CND_ARTIFACT_NAME_Release=mcdonaldjohnpaul_final_csc-17c_problem7_simplelinked
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_1-Windows/mcdonaldjohnpaul_final_csc-17c_problem7_simplelinked
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_1-Windows/package
+CND_PACKAGE_NAME_Release=mcdonaldjohnpaulfinalcsc-17cproblem7simplelinked.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_1-Windows/package/mcdonaldjohnpaulfinalcsc-17cproblem7simplelinked.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
